8bcb543b1793901cac934d4a5835411ee5bb2c37
Jesús <jesuslopj0@gmail.com>
2026-06-12 14:40:02 +0200

    Nuevo og image, quitando navbar ppal de carta y botones de compartir

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://casinoelbonillo.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
